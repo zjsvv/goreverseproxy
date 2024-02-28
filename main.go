d9970cccddb13ca96e5e78e6103e66ce9f851174
zjs <jinsheng358@gmail.com>
2024-02-27 16:28:10 -0800

    Basic reverse proxy

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os/signal"
 	"syscall"
 	"time"
@@ -11,17 +13,33 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func proxy(c *gin.Context) {
+	remote, err := url.Parse("http://localhost:9000/")
+	if err != nil {
+		panic(err)
+	}
+
+	director := func(req *http.Request) {
+		req.Header.Add("X-Forwarded-Host", req.Host)
+		req.Header.Add("X-Origin-Host", remote.Host)
+		req.URL.Scheme = remote.Scheme
+		req.URL.Host = remote.Host
+	}
+
+	proxy := &httputil.ReverseProxy{Director: director}
+
+	proxy.ServeHTTP(c.Writer, c.Request)
+}
+
 func main() {
 	// Create context that listens for the interrupt signal from the OS.
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Welcome Gin Server")
-	})
+	router.GET("/", proxy)
 
-	srv := &http.Server{
+	server := &http.Server{
 		Addr:    ":8080",
 		Handler: router,
 	}
@@ -29,7 +47,7 @@ func main() {
 	// Initializing the server in a goroutine so that
 	// it won't block the graceful shutdown handling below
 	go func() {
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("listen: %s\n", err)
 		}
 	}()
@@ -45,9 +63,9 @@ func main() {
 	// the request it is currently handling
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	if err := srv.Shutdown(ctx); err != nil {
+	if err := server.Shutdown(ctx); err != nil {
 		log.Fatal("Error while shutting down Server. Server forced to shutdown: ", err)
- 	}
+	}
 
 	log.Println("Server exiting")
 }
