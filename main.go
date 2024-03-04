122f1089e44f9ca4b5ea1dc41f3a4ed80716e21e
zjs <jinsheng358@gmail.com>
2024-03-04 14:38:21 -0800

    Use net/http instead

--- main.go
+++ main.go
@@ -1,53 +1,130 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"io"
 	"log"
+	"log/slog"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
 	"os/signal"
 	"syscall"
 	"time"
-
-	"github.com/gin-gonic/gin"
 )
 
-func proxy(c *gin.Context) {
-	remote, err := url.Parse("http://localhost:9000/")
+type RevProxy struct {
+	context context.Context
+	target  *url.URL
+	proxy   *httputil.ReverseProxy
+}
+
+func (rp *RevProxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	// Do anything you want here
+	// e.g. blacklisting IP, log time, modify headers, etc
+
+	log.Printf("Proxy receives request.")
+	log.Printf("Proxy forwards request to origin.")
+
+	req.Host = rp.target.Host
+	// rp.recordRequest(r)
+	rp.proxy.ServeHTTP(rw, req)
+
+	log.Printf("Origin server completes request.")
+}
+
+func (rp *RevProxy) recordRequest(req *http.Request) {
+	header := req.Header
+
+	var reqBody []byte
+	if body, err := io.ReadAll(req.Body); err == nil {
+		reqBody = body
+		req.Body = io.NopCloser(bytes.NewBuffer(reqBody))
+		log.Println(reqBody)
+	}
+
+	cloneHeader := header.Clone()
+
+	headerJSON, err := json.Marshal(cloneHeader)
 	if err != nil {
-		panic(err)
+		log.Println("Marshal header failed", "err", err)
+		return
 	}
 
-	director := func(req *http.Request) {
-		req.Header.Add("X-Forwarded-Host", req.Host)
-		req.Header.Add("X-Origin-Host", remote.Host)
-		req.URL.Scheme = remote.Scheme
-		req.URL.Host = remote.Host
+	slog.Info("Record request",
+		slog.Int64("timestamp", time.Now().Unix()),
+		slog.String("method", req.Method),
+		slog.String("path", req.URL.Path),
+		slog.String("query", req.URL.RawQuery),
+		slog.String("header", string(headerJSON)),
+		slog.String("Body", string(reqBody)),
+	)
+}
+
+func modifyRequest(req *http.Request) {
+	req.Header.Set("X-Proxy", "Simple-Reverse-Proxy")
+	log.Println("--------- modify Request -----------")
+}
+
+func modifyResponse() func(*http.Response) error {
+	return func(resp *http.Response) error {
+		resp.Header.Set("X-Proxy", "Magical")
+		log.Println("--------- modify Response -----------")
+		return nil
+	}
+}
+
+func NewRevProxy(ctx context.Context, rawUrl string) (*RevProxy, error) {
+	remote, err := url.Parse(rawUrl)
+	if err != nil {
+		return nil, err
 	}
 
-	proxy := &httputil.ReverseProxy{Director: director}
+	s := &RevProxy{
+		context: ctx,
+		target:  remote,
+		proxy:   httputil.NewSingleHostReverseProxy(remote),
+	}
 
-	proxy.ServeHTTP(c.Writer, c.Request)
+	// Modify requests
+	originalDirector := s.proxy.Director
+	s.proxy.Director = func(r *http.Request) {
+		originalDirector(r)
+		modifyRequest(r)
+	}
+
+	// Modify response
+	s.proxy.ModifyResponse = modifyResponse()
+
+	return s, nil
 }
 
 func main() {
+	// Set the logger for the application
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
 	// Create context that listens for the interrupt signal from the OS.
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
-	router := gin.Default()
-	router.GET("/", proxy)
+	revProxy, err := NewRevProxy(context.Background(), "http://localhost:9000")
+	if err != nil {
+		panic(err)
+	}
 
-	server := &http.Server{
+	srv := &http.Server{
 		Addr:    ":8080",
-		Handler: router,
+		Handler: revProxy,
 	}
 
 	// Initializing the server in a goroutine so that
 	// it won't block the graceful shutdown handling below
 	go func() {
-		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("listen: %s\n", err)
 		}
 	}()
@@ -63,7 +140,7 @@ func main() {
 	// the request it is currently handling
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	if err := server.Shutdown(ctx); err != nil {
+	if err := srv.Shutdown(ctx); err != nil {
 		log.Fatal("Error while shutting down Server. Server forced to shutdown: ", err)
 	}
 
