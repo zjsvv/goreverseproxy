c2d3cc5574d4d5accb8016822456a8c00ca582ac
zjs <jinsheng358@gmail.com>
2024-07-28 21:06:04 -0700

    Add logging middleware

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
-	"io"
 	"log"
 	"log/slog"
 	"net/http"
@@ -14,6 +11,8 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/zjsvv/goreverseproxy/middleware"
 )
 
 type RevProxy struct {
@@ -22,57 +21,24 @@ type RevProxy struct {
 	proxy   *httputil.ReverseProxy
 }
 
-func (rp *RevProxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	// Do anything you want here
-	// e.g. blacklisting IP, log time, modify headers, etc
-
-	log.Printf("Proxy receives request.")
-	log.Printf("Proxy forwards request to origin.")
+func (rp *RevProxy) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	slog.Info("[RevProxy][ServeHTTP] Proxy is going to forward request to origin.")
 
 	req.Host = rp.target.Host
-	// rp.recordRequest(r)
-	rp.proxy.ServeHTTP(rw, req)
+	rp.proxy.ServeHTTP(w, req)
 
-	log.Printf("Origin server completes request.")
-}
-
-func (rp *RevProxy) recordRequest(req *http.Request) {
-	header := req.Header
-
-	var reqBody []byte
-	if body, err := io.ReadAll(req.Body); err == nil {
-		reqBody = body
-		req.Body = io.NopCloser(bytes.NewBuffer(reqBody))
-		log.Println(reqBody)
-	}
-
-	cloneHeader := header.Clone()
-
-	headerJSON, err := json.Marshal(cloneHeader)
-	if err != nil {
-		log.Println("Marshal header failed", "err", err)
-		return
-	}
-
-	slog.Info("Record request",
-		slog.Int64("timestamp", time.Now().Unix()),
-		slog.String("method", req.Method),
-		slog.String("path", req.URL.Path),
-		slog.String("query", req.URL.RawQuery),
-		slog.String("header", string(headerJSON)),
-		slog.String("Body", string(reqBody)),
-	)
+	slog.Info("[RevProxy][ServeHTTP] Origin server completes request.")
 }
 
 func modifyRequest(req *http.Request) {
+	slog.Info("[RevProxy][modifyRequest]")
 	req.Header.Set("X-Proxy", "Simple-Reverse-Proxy")
-	log.Println("--------- modify Request -----------")
 }
 
 func modifyResponse() func(*http.Response) error {
 	return func(resp *http.Response) error {
+		slog.Info("[RevProxy][modifyResponse]")
 		resp.Header.Set("X-Proxy", "Magical")
-		log.Println("--------- modify Response -----------")
 		return nil
 	}
 }
@@ -111,14 +77,16 @@ func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
-	revProxy, err := NewRevProxy(context.Background(), "http://localhost:9000")
+	origin := "http://localhost:9000"
+
+	revProxy, err := NewRevProxy(context.Background(), origin)
 	if err != nil {
 		panic(err)
 	}
 
 	srv := &http.Server{
 		Addr:    ":8080",
-		Handler: revProxy,
+		Handler: middleware.NewLogger(revProxy),
 	}
 
 	// Initializing the server in a goroutine so that
@@ -134,7 +102,7 @@ func main() {
 
 	// Restore default behavior on the interrupt signal and notify user of shutdown.
 	stop()
-	log.Println("shutting down gracefully, press Ctrl+C again to force")
+	slog.Info("shutting down gracefully, press Ctrl+C again to force")
 
 	// The context is used to inform the server it has 5 seconds to finish
 	// the request it is currently handling
@@ -144,5 +112,5 @@ func main() {
 		log.Fatal("Error while shutting down Server. Server forced to shutdown: ", err)
 	}
 
-	log.Println("Server exiting")
+	slog.Info("Server exiting")
 }
---
+++ middleware/middleware.go
@@ -0,0 +1,117 @@
+package middleware
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"log"
+	"log/slog"
+	"net/http"
+	"time"
+)
+
+// struct for holding response details
+type responseData struct {
+	status int
+	size   int
+	body   *bytes.Buffer
+}
+
+// custom http.ResponseWriter implementation
+type loggingResponseWriter struct {
+	http.ResponseWriter // compose original http.ResponseWriter
+	responseData        *responseData
+}
+
+func (lrw *loggingResponseWriter) Write(b []byte) (int, error) {
+	size, err := lrw.ResponseWriter.Write(b) // write response using original http.ResponseWriter
+	lrw.responseData.size += size            // capture size
+	lrw.responseData.body.Write(b)
+	return size, err
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(statusCode int) {
+	lrw.ResponseWriter.WriteHeader(statusCode) // write status code using original http.ResponseWriter
+	lrw.responseData.status = statusCode       // capture status code
+}
+
+func (lrw *loggingResponseWriter) Header() http.Header {
+	return lrw.ResponseWriter.Header()
+}
+
+// Logger is a middleware handler that does request logging
+type Logger struct {
+	Handler http.Handler
+}
+
+// ServeHTTP handles the request by passing it to the real
+// handler and logging the request details
+func (l *Logger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+
+	responseData := &responseData{
+		status: 0,
+		size:   0,
+		body:   bytes.NewBuffer(nil),
+	}
+	lrw := loggingResponseWriter{
+		ResponseWriter: w, // compose original http.ResponseWriter
+		responseData:   responseData,
+	}
+
+	recordRequest(r)
+
+	slog.Info("[middleware][ServeHTTP]")
+	l.Handler.ServeHTTP(&lrw, r)
+
+	recordResponse(lrw, time.Since(start))
+}
+
+// NewLogger constructs a new Logger middleware handler
+func NewLogger(handlerToWrap http.Handler) *Logger {
+	return &Logger{handlerToWrap}
+}
+
+func recordRequest(req *http.Request) {
+	header := req.Header
+
+	var reqBody []byte
+	if body, err := io.ReadAll(req.Body); err != nil {
+		reqBody = body
+		req.Body = io.NopCloser(bytes.NewBuffer(reqBody))
+		log.Println(reqBody)
+	}
+
+	cloneHeader := header.Clone()
+
+	headerJSON, err := json.Marshal(cloneHeader)
+	if err != nil {
+		log.Printf("json.Marshal header failed. err: %+s\n", err)
+	}
+
+	slog.Info("[middleware][recordRequest]")
+	slog.Info("Record request",
+		slog.Int64("timestamp", time.Now().Unix()),
+		slog.String("method", req.Method),
+		slog.String("path", req.URL.Path),
+		slog.String("query", req.URL.RawQuery),
+		slog.String("header", string(headerJSON)),
+		slog.String("Body", string(reqBody)),
+	)
+}
+
+func recordResponse(lrw loggingResponseWriter, duration time.Duration) {
+	headerJSON, err := json.Marshal(lrw.Header())
+	if err != nil {
+		log.Printf("json.Marshal header failed. err: %+s\n", err)
+	}
+
+	slog.Info("[middleware] [recordResponse]")
+	slog.Info("Request completed",
+		slog.Int("status", lrw.responseData.status),
+		slog.Int("size", lrw.responseData.size),
+		slog.Int64("duration(ms)", duration.Milliseconds()),
+		slog.String("header", string(headerJSON)),
+		slog.String("body", string(lrw.responseData.body.String())),
+	)
+}
